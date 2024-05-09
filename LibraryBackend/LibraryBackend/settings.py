509379f2ac7423958c05417839a37d3bb7c27338
Adrian <aaadrianrutkaaa@o2.pl>
2024-05-09 18:47:00 +0200

    add book app to installed apps and create new directory for book covers

--- LibraryBackend/LibraryBackend/settings.py
+++ LibraryBackend/LibraryBackend/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'book',
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders'
