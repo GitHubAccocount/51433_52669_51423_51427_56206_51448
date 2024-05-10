2a88806f01214de7fe2d5beb3805288ddf643c1b
Adrian <aaadrianrutkaaa@o2.pl>
2024-05-10 23:43:12 +0200

    add media url so frontend can retrive images

--- LibraryBackend/LibraryBackend/settings.py
+++ LibraryBackend/LibraryBackend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -146,6 +148,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- LibraryBackend/LibraryBackend/urls.py
+++ LibraryBackend/LibraryBackend/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
   path('api/', include('account.urls')),
-    path('admin/', admin.site.urls),
-]
+  path('api/books/', include('book.urls')),
+  path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
