9e74442ea3de0761a36ddd533c38fbcac6eb1273
Adrian <aaadrianrutkaaa@o2.pl>
2024-05-10 23:45:07 +0200

    add request so it can be used in serializers

--- LibraryBackend/book/views.py
+++ LibraryBackend/book/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+
+@api_view(['GET'])
+def book_list(request: Request):
+  books = Book.objects.all()
+
+  serializer = BookSerializer(books, many=True, context={'request': request})
+
+  return JsonResponse({'books': serializer.data})
