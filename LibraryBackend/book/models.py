7d09282b6b2a712313222674d0fbca24a7ffe75c
Adrian <aaadrianrutkaaa@o2.pl>
2024-05-12 11:47:19 +0200

    feat: add default path to image

--- LibraryBackend/book/models.py
+++ LibraryBackend/book/models.py
@@ -3,11 +3,14 @@ import uuid
 
 import os
 
+def default_image_path():
+    return 'covers/defaultCover.png'  # Path to the default photo
+
 class Book(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   title = models.CharField(max_length=255, blank=False)
   author = models.CharField(max_length=100, blank=False)
-  image = models.ImageField(upload_to='covers/', blank=True, null=True)
+  image = models.ImageField(upload_to='covers/', blank=True, null=True, default=default_image_path)
   year_published = models.PositiveIntegerField(blank=True, null=True)
   category = models.CharField(max_length=100, blank=True)
   description = models.TextField(blank=True)
