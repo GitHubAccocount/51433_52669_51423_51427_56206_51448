bbf0e5f88aba9b8efb40bfe527ad627ad5d7869a
MarcinDziurdzia <122217161+MarcinDziurdzia@users.noreply.github.com>
2024-05-19 18:19:09 +0200

    Added LoanSerializer in serializers.py

--- LibraryBackend/book/serializers.py
+++ LibraryBackend/book/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
-from .models import Book
+from.models import Book, Loan
 
 class BookSerializer(serializers.ModelSerializer):
-  image_url = serializers.SerializerMethodField()
+    image_url = serializers.SerializerMethodField()
 
-  class Meta:
-    model = Book
-    fields = '__all__'
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'author', 'publication_date', 'image_url']
 
-  def get_image_url(self, obj):
-    if obj.image:
-        return self.context['request'].build_absolute_uri(obj.image.url)
-    return None
+    def get_image_url(self, obj):
+        if obj.image:
+            return self.context['request'].build_absolute_uri(obj.image.url)
+        return None
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Loan
+        fields = ['id', 'book', 'borrower', 'eserved_at', 'eturned_at']
--- LibraryBackend/book/views.py
+++ LibraryBackend/book/views.py
@@ -1,27 +1,35 @@
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 
 from.models import Book, Loan
 from.serializers import BookSerializer, LoanSerializer
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def book_list(request: Request):
     if request.method == 'GET':
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True, context={'request': request})
-        return JsonResponse({'books': serializer.data})
+        return Response({'books': serializer.data})
     elif request.method == 'POST':
         data = request.data
-        book_id = data['book_id']
-        book = Book.objects.get(id=book_id)
-        loan = Loan(book=book, borrower=request.user)
-        loan.reserve()
-        serializer = LoanSerializer(loan, context={'request': request})
-        return JsonResponse(serializer.data)
+        if 'book_id' not in data:
+            return Response({'error': 'Book ID is required'}, status=400)
+        try:
+            book_id = data['book_id']
+            book = Book.objects.get(id=book_id)
+            loan = Loan(book=book, borrower=request.user)
+            loan.reserve()
+            serializer = LoanSerializer(loan, context={'request': request})
+            return Response(serializer.data)
+        except Book.DoesNotExist:
+            return Response({'error': 'Book not found'}, status=404)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def loan_list(request: Request):
     loans = Loan.objects.all()
     serializer = LoanSerializer(loans, many=True, context={'request': request})
-    return JsonResponse({'loans': serializer.data})
+    return Response({'loans': serializer.data})
