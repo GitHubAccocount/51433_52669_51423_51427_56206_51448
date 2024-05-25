828932314d97c82f129179595ca04391349cd524
MarcinDziurdzia <122217161+MarcinDziurdzia@users.noreply.github.com>
2024-05-25 15:25:07 +0200

    Added Password change function

--- LibraryBackend/LibraryBackend/urls.py
+++ LibraryBackend/LibraryBackend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+from account import views
 
 urlpatterns = [
   path('api/', include('account.urls')),
   path('api/books/', include('book.urls')),
   path('admin/', admin.site.urls),
+  path('api/change-password/', views.change_password, name='change_password'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
